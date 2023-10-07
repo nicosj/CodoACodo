70fc068eed837519968b737d249a0813812db97f
https://github.com/BelenAlegrette <108761268+BelenAlegrette@users.noreply.github.com>
2023-10-07 11:38:29 -0300

    Cambio de Rama Bel

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+const headerCarousel = document.getElementById('headerCarousel');
+let currentIndex = 0;
+
+function showImage(index) {
+    headerCarousel.style.transform = `translateX(${-index * 100}%)`;
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % headerCarousel.children.length;
+    showImage(currentIndex);
+}
+
+setInterval(nextImage, 1000); 
