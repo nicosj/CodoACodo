650b1ee421a91876a136d3908cd701edb6b42de2
https://github.com/BelenAlegrette <108761268+BelenAlegrette@users.noreply.github.com>
2023-10-31 15:38:47 -0300

    Reorganiza validación form

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,3 +14,28 @@ function cerrarModal() {
     
     modal.style.display = "none";
 }
+
+
+/* API */
+const URL =
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/735173/rpg-2-data.json";
+import { Imagen } from "./Imagen.js";
+import { Mostrar } from "./Mostrar.js";
+
+fetch(URL, {})
+  .then((res) => res.json())
+  .then((data) => {
+    localStorage.setItem("img", JSON.stringify(data.gallery));
+  })
+  .catch((error) => log({ error }));
+
+let mans = JSON.parse(localStorage.getItem("img"));
+mans.forEach((items) => {
+  const contenedor = new Mostrar("galeria");
+  let id = items.id;
+  let name = items.name;
+  let desc = items.desc;
+  let src = items.src;
+  const img = new Imagen({ id, name, desc, src });
+  contenedor.render(img.src);
+});
